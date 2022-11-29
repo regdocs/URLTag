82aa8a8942225461909b4c2e04f968fcfb057198
jayzsh <jay.dnb@outlook.in>
2022-11-29 10:16:59 +0530

    Translate syntax commonjs -> module

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,26 +1,30 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+import { Db, MongoClient } from "mongodb";
 
-var mongoClient: typeof MongoClient;
+var mongoClient: MongoClient;
 
-module.exports = {
-    connectToCluster: () => {
-        console.log('Connecting to your URLTag Analytics cluster...');
+export const mongoConfig = {
+    connectToCluster: async (): Promise<MongoClient> => {
+        console.log("Connecting to your URLTag Analytics cluster...");
         try {
-            mongoClient = new MongoClient(
-                process.env.MONGODB_URI, 
-                { 
-                    useNewUrlParser: true, 
-                    useUnifiedTopology: true, 
-                    serverApi: ServerApiVersion.v1
-                }
-            );
-            console.log('🗸 Connected to Mongo cluster \n');
+            mongoClient = new MongoClient(process.env.MONGODB_URI!, {
+                monitorCommands: true,
+            });
+
+            console.log("🗸 Connected to Mongo cluster \n");
+            const db: Db = mongoClient.db("test");
+
+            if ((await db.collections()).length < 1) {
+                console.log('Creating collection test["batches"]...');
+                await db.createCollection("batches");
+            }
+
             return mongoClient;
         } catch (e) {
-            console.log('✗ Connection to Mongo cluster failed: Exiting... ' + e + '\n');
+            console.log(
+                "✗ Connection to Mongo cluster failed: Exiting... " + e + "\n"
+            );
             process.exit(1);
         }
     },
-    getMongoClient: () => mongoClient
+    getMongoClient: () => mongoClient,
 };
