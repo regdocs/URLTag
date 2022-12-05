d90d8e445cfbe6b17ac3735fe38b8f7505d8fd9d
jayzsh <jay.dnb@outlook.in>
2022-12-05 10:30:40 +0530

    Chore:reformat; add express-router redirects from root

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,39 @@
+import * as moduleGenerate from "./generate";
+import * as moduleListener from "./listener";
+import * as moduleAdmin from "./admin";
+
 const cors = require("cors");
 const express = require("express");
+
 require("dotenv").config("../.env");
-// require('./config/mongodb').connectToCluster();
+require("./config/mongodb").mongoConfig.connectToCluster();
 
 const app = express();
-app.disable('x-powered-by');
-app.use(express.json({ limit: '5mb' }));
+app.disable("x-powered-by");
+app.use(express.json({ limit: "5mb" }));
 app.use(cors());
 
 const uptimeInit = Date.now();
 const sayUptime = () => {
     const ms = Date.now() - uptimeInit;
-    const days = Math.floor(ms / (24*60*60*1000));
-    const daysms = ms % (24*60*60*1000);
-    const hours = Math.floor(daysms / (60*60*1000));
-    const hoursms = ms % (60*60*1000);
-    const minutes = Math.floor(hoursms / (60*1000));
-    const minutesms = ms % (60*1000);
+    const days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    const daysms = ms % (24 * 60 * 60 * 1000);
+    const hours = Math.floor(daysms / (60 * 60 * 1000));
+    const hoursms = ms % (60 * 60 * 1000);
+    const minutes = Math.floor(hoursms / (60 * 1000));
+    const minutesms = ms % (60 * 1000);
     const sec = Math.floor(minutesms / 1000);
     return (
-          (days ? days + " day(s) " : "")
-        + (hours ? hours + " hour(s) " : "")
-        + (minutes ? minutes + " min(s) " : "")
-        + (sec ? sec + " sec " : "")
+        (days ? days + " day(s) " : "") +
+        (hours ? hours + " hour(s) " : "") +
+        (minutes ? minutes + " min(s) " : "") +
+        (sec ? sec + " sec " : "")
     );
 };
 
-app.get('/', async function (req: any, res: any) {
+app.get("/", async function (req: any, res: any) {
     res.send(`
-<h1><u><pre>URLTag Analytics</u></pre></h1><pre>
+<h1><u><pre>URLTag</u></pre></h1><pre>
 <a href="https://github.com/jayzsh/urltag">https://github.com/jayzsh/urltag</a><br \>
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
@@ -60,7 +65,8 @@ API uptime: <b>${sayUptime()}</b></pre>
     `);
 });
 
-app.use("/urltag/generate", require("./generate"));
-app.use("/urltag/emit", require("./listener"));
+app.use("/urltag/generate", moduleGenerate.router);
+app.use("/urltag/emit", moduleListener.router);
+app.use("/urltag/inspect", moduleAdmin.router);
 
 app.listen(process.env.PORT || 4000);
